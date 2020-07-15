76e17cf8d28fe087fb165af308953926c6124f0e
Aris Markogiannakis <aris.markogiannakis@gmail.com>
2020-07-15 21:42:06 +0300

    starter

---
+++ components/competitions.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        competitions
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass" scoped>
+  
+
+</style>
---
+++ components/live.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        live
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass" scoped>
+  
+
+</style>
